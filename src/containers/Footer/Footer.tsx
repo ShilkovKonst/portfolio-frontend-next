2a978ab15eac01919de7f0d4224f35a5b4572849
Konstantin Shilkov <konst.shilkov@gmail.com>
2023-08-03 21:41:47 +0200

    11

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -20,60 +20,62 @@ const Footer = () => {
 
   const { name, email, message } = formData
 
-  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target
     setFormData({ ...formData, [name]: value })
   }
 
   const handleSubmit = () => {
-    setIsLoading(true)
-    const contact = {
-      _type: 'contact',
-      name: name,
-      email: email,
-      message: message
-    }
+    if (name != '' && email != '' && message != '') {
+      setIsLoading(true)
+      const contact = {
+        _type: 'contact',
+        name: name,
+        email: email,
+        message: message
+      }
 
-    client.create(contact).then(() => {
-      setIsLoading(false)
-      setIsSubmitted(true)
-    })
+      client.create(contact).then(() => {
+        setIsLoading(false)
+        setIsSubmitted(true)
+      })
+    }
   }
 
   return (
     <>
-      <h2 className='head-text'> Contact me</h2>
+      <h2 className='head-text'>Contactez-Moi</h2>
       <div className='app__footer-cards'>
         <div className='app__footer-card'>
           <Image src={images.mail} alt='mail' width={512} height={512} />
           <Link className='p-text' href='mailto: konst.shilkov@gmail.com'>
-            by Email: konst.shilkov@gmail.com
+            via Email: konst.shilkov@gmail.com
           </Link>
         </div>
         <div className='app__footer-card'>
           <Image src={images.phone} alt='phone' width={512} height={512} />
           <Link className='p-text' href='tel: +33668476320'>
-            by Phone: +33 6 68 47 63 20
+            via Phone: +33 6 68 47 63 20
           </Link>
         </div>
       </div>
       {!isSubmitted ?
-        <form className='app__footer-form app__flex'>
-          <h4 className='subhead-text'>Or by using this form</h4>
+        <form onSubmit={handleSubmit} className='app__footer-form app__flex'>
+          <h4 className='subhead-text'>Ou en utilisant cette forme</h4>
           <div className='app__flex'>
-            <input className='p-text' type="text" placeholder='Your name' name='name' value={name} onChange={handleChangeInput} />
+            <input className='p-text' required type="text" placeholder='Votre nom' name='name' value={name} onChange={handleChangeInput} />
           </div>
           <div className='app__flex'>
-            <input className='p-text' type="text" placeholder='Your email' name='email' value={email} onChange={handleChangeInput} />
+            <input className='p-text' required type="text" placeholder='Votre email' name='email' value={email} onChange={handleChangeInput} />
           </div>
           <div>
-            <textarea className='p-text' placeholder='Your message' name="message" value={message} onChange={handleChangeInput} />
+            <textarea className='p-text' required placeholder='Votre message' name="message" value={message} onChange={handleChangeInput} />
           </div>
-          <button type='button' onClick={handleSubmit}>{isLoading ? 'Sending...' : 'Send message'}</button>
+          <button type='submit' onClick={handleSubmit}>{isLoading ? 'Envoi en cours...' : 'Envoyer le message'}</button>
         </form>
         :
         <div>
-          <h3 className='head-text'>{`Thank you, ${name}, for contact me!`}</h3>
+          <h3 className='head-text'>{`Merci, ${name}, pour me contacter!`}</h3>
         </div>
       }
 
